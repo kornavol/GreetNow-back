8f6b6ecb86e6b8e4c2d3016a738e67f2d83df9b1
Restyled.io <commits@restyled.io>
2021-08-11 12:39:24 +0000

    Restyled by prettier

--- router/cards.js
+++ router/cards.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const cardsControl = require('../controller/cards')
+const cardsControl = require("../controller/cards");
 // const {convertIds} = require('../middleware/namesToId')
 
 router.get("/getCard", cardsControl.getCard);
